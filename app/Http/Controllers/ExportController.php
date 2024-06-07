796e039acb5ea4b00b5ebe46d7be1b344500cbfb
RY1997 <riskydharmayudistira@gmail.com>
2024-06-07 08:57:44 +0700

    "Added 'nama_pemda' to orderBy clause in SuratTugas query in ExportController's progres method."

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -36,7 +36,7 @@ class ExportController extends AppBaseController
 
     public function progres()
     {
-        $suratTugas = SuratTugas::where('jenis_tkd', session('jenis_tkd'))->orderBy('kode_pwk')->orderBy('jenis_penugasan', 'DESC')
+        $suratTugas = SuratTugas::where('jenis_tkd', session('jenis_tkd'))->orderBy('kode_pwk')->orderBy('nama_pemda', 'DESC')->orderBy('jenis_penugasan', 'DESC')
         ->withCount(['apbd as belum_apbd' => function ($query) {
             $query->where('belanja_pegawai', '=', 0);
         }])
