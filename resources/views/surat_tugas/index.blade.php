20828775255d0cdcb44dc614f6d31c6aa761cacf
RY1997 <riskydharmayudistira@gmail.com>
2024-09-11 16:31:32 +0700

    .

--- resources/views/surat_tugas/index.blade.php
+++ resources/views/surat_tugas/index.blade.php
@@ -25,7 +25,7 @@ Pelaksanaan Penugasan
         </form>
     </div>
     <div class="col-lg-1 col-1 text-right">
-        <a class="btn btn-success float-right" href="{{ route('suratTugas.create') }}">
+        <a class="btn btn-success float-right mx-1" href="{{ route('suratTugas.create') }}">
             Tambah
         </a>
     </div>
