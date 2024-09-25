d1e1fddf5b950ad05d2e935c25232943e73f69f3
RY1997 <riskydharmayudistira@gmail.com>
2024-09-26 01:00:49 +0700

    "Removed AVG(CASE WHEN target_output != 0 THEN capaian_output / target_output ELSE 0 END) as rerata_capaian from one query and added it to another query in EvaluasiTrenController.php"

--- app/Http/Controllers/EvaluasiTrenController.php
+++ app/Http/Controllers/EvaluasiTrenController.php
@@ -60,7 +60,7 @@ class EvaluasiTrenController extends AppBaseController
         $monitoringPenyalurans = EvaluasiPenyaluran::whereIn('nama_pemda', $monitoringTrens->pluck('nama_pemda'))->selectRaw('*, SUM(penyaluran_tkd) as total_penyaluran')->where('jenis_tkd', session('jenis_tkd'))
             ->groupBy('nama_pemda')->groupBy('tahun')->get();
 
-        $monitoringPenggunaans = EvaluasiPenggunaan::whereIn('nama_pemda', $monitoringTrens->pluck('nama_pemda'))->selectRaw('*, SUM(anggaran_barjas + anggaran_pegawai + anggaran_modal + anggaran_hibah + anggaran_lainnya + anggaran_na) as total_anggaran, SUM(realisasi_barjas + realisasi_pegawai + realisasi_modal + realisasi_hibah + realisasi_lainnya + realisasi_na) as total_realisasi, AVG(CASE WHEN target_output != 0 THEN capaian_output / target_output ELSE 0 END) as rerata_capaian')
+        $monitoringPenggunaans = EvaluasiPenggunaan::whereIn('nama_pemda', $monitoringTrens->pluck('nama_pemda'))->selectRaw('*, SUM(anggaran_barjas + anggaran_pegawai + anggaran_modal + anggaran_hibah + anggaran_lainnya + anggaran_na) as total_anggaran, SUM(realisasi_barjas + realisasi_pegawai + realisasi_modal + realisasi_hibah + realisasi_lainnya + realisasi_na) as total_realisasi')
             ->where('jenis_tkd', session('jenis_tkd'))
             ->groupBy('nama_pemda')->groupBy('tahun')->get();
 
@@ -182,7 +182,7 @@ class EvaluasiTrenController extends AppBaseController
         $monitoringPenggunaans = EvaluasiPenggunaan::where('tahun', $pemda->tahun)
             ->where('nama_pemda', $pemda->nama_pemda)
             ->where('jenis_tkd', session('jenis_tkd'))
-            ->selectRaw('*, SUM(anggaran_barjas + anggaran_pegawai + anggaran_modal + anggaran_hibah + anggaran_lainnya + anggaran_na) as total_anggaran, SUM(realisasi_barjas + realisasi_pegawai + realisasi_modal + realisasi_hibah + realisasi_lainnya + realisasi_na) as total_realisasi')
+            ->selectRaw('*, SUM(anggaran_barjas + anggaran_pegawai + anggaran_modal + anggaran_hibah + anggaran_lainnya + anggaran_na) as total_anggaran, SUM(realisasi_barjas + realisasi_pegawai + realisasi_modal + realisasi_hibah + realisasi_lainnya + realisasi_na) as total_realisasi, AVG(CASE WHEN target_output != 0 THEN capaian_output / target_output ELSE 0 END) as rerata_capaian')
             ->groupBy('bidang_tkd', 'subbidang_tkd')->get();
 
         return view('evaluasi_trens.show')->with([
