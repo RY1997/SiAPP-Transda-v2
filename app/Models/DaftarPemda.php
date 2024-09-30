f649921729d4055f23fc878fb877206bbc151a8c
RY1997 <riskydharmayudistira@gmail.com>
2024-09-30 12:00:21 +0700

    .

--- app/Models/DaftarPemda.php
+++ app/Models/DaftarPemda.php
@@ -14,4 +14,49 @@ class DaftarPemda extends Model
     public $fillable = [
         'antrian',
     ];
+
+    public function dataUmumTkd()
+    {
+        return $this->hasMany(\App\Models\DataUmumTkd::class, 'nama_pemda', 'nama_pemda');
+    }
+
+    public function monitoringApbd()
+    {
+        return $this->hasMany(\App\Models\MonitoringApbd::class, 'nama_pemda', 'nama_pemda');
+    }
+
+    public function monitoringAlokasi()
+    {
+        return $this->hasMany(\App\Models\MonitoringAlokasi::class, 'nama_pemda', 'nama_pemda');
+    }
+
+    public function monitoringPenyaluran()
+    {
+        return $this->hasMany(\App\Models\MonitoringPenyaluran::class, 'nama_pemda', 'nama_pemda');
+    }
+
+    public function monitoringPenggunaan()
+    {
+        return $this->hasMany(\App\Models\MonitoringPenggunaan::class, 'nama_pemda', 'nama_pemda');
+    }
+
+    public function monitoringSisaTkd()
+    {
+        return $this->hasMany(\App\Models\MonitoringSisaTkd::class, 'nama_pemda', 'nama_pemda');
+    }
+
+    public function monitoringImmediateOutcome()
+    {
+        return $this->hasMany(\App\Models\MonitoringImmediateOutcome::class, 'nama_pemda', 'nama_pemda');
+    }
+
+    public function monitoringHibah()
+    {
+        return $this->hasMany(\App\Models\MonitoringHibah::class, 'nama_pemda', 'nama_pemda');
+    }
+
+    public function monitoringIndikatorMakro()
+    {
+        return $this->hasMany(\App\Models\MonitoringIndikatorMakro::class, 'nama_pemda', 'nama_pemda');
+    }
 }
